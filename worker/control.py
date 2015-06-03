d580cd4779f21b5906513aa1b43f4e8a38b4dafa
andrewosh <andrewosh@gmail.com>
2015-06-03 19:16:55 -0400

    Added an additional config option for the host name

--- worker/control.py
+++ worker/control.py
@@ -3,23 +3,22 @@ import yaml
 import json
 import requests
 
-PUBLIC_IP_URL = "http://169.254.169.254/latest/meta-data/public-ipv4"
 CONF_FILE = "conf.yml"
 
-def get_public_ip(settings): 
-    return 'http://' + requests.get(PUBLIC_IP_URL).text + ':' + str(settings['worker']['port'])
+def get_host_and_port(settings): 
+    return settings['worker']['host'] + ':' + str(settings['worker']['port'])
 
 def get_master_url(settings): 
     return settings['master']['host'] + ':' + str(settings['master']['port']) + '/hosts'
 
 def start(settings): 
-    public_ip = get_public_ip(settings)
+    public_ip = get_host_and_port(settings)
     url = get_master_url(settings)
     payload = {"host": public_ip}
     requests.post(url, data=json.dumps(payload))
 
 def stop(settings): 
-    public_ip = get_public_ip(settings)
+    public_ip = get_host_and_port(settings)
     url = get_master_url(settings)
     payload = {"host": public_ip}
     requests.delete(url, data=json.dumps(payload))
