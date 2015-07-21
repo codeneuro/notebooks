f4dddb2eeaf13575e93ddb8e42948332cc1a61e4
freeman <the.freeman.lab@gmail.com>
2015-07-21 00:31:12 -0400

    Force target to self

--- worker/docker/base/profile_default/static/custom/custom.js
+++ worker/docker/base/profile_default/static/custom/custom.js
@@ -4,8 +4,9 @@ IPython._target = '_self';
 
 require(["base/js/events"], function (events) {
     events.on("notebook_loaded.Notebook", function () {
-        // disable warn-on-unsaved changes
-        // these are ephemeral notebooks, nobody should have any work to lose
+        // disable warn-on-unsaved changes as these are ephemeral notebooks
         window.onbeforeunload = null;
+        // additional custom js to force external links to stay in same window
+        $('a').attr('target','_self')
     });
 });
